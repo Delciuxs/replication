6bc5e652da753429f804710ddcb8e9606eb14131
CobraUltimate <carrocobra2@hotmail.com>
2021-06-16 11:38:20 -0500

    - Limpieza.

--- Backend/routes/servidoresRoutes.js
+++ Backend/routes/servidoresRoutes.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const fetch = require("node-fetch");
 const router = express.Router();
-const Calificacion = require("../model/calificacionesModel");
 const Servidor = require("../model/servidoresModel");
 
 //Obtener todas las calificaciones
@@ -35,11 +34,6 @@ router.get("/", async (req, res) => {
   }
 });
 
-//Obtener una calificacion
-router.get("/:id", getCalificacion, (req, res) => {
-  res.json(res.calificacion);
-});
-
 router.post("/", async (req, res) => {
   try {
     const servidor = new Servidor(req.body);
@@ -49,111 +43,5 @@ router.post("/", async (req, res) => {
     res.status(400).json({ message: error.message });
   }
 });
-
-//Actualizando calificacion
-router.patch("/:id", getCalificacion, async (req, res) => {
-  if (req.body.noBoleta != null) {
-    res.calificacion.noBoleta = req.body.noBoleta;
-  }
-  if (req.body.nombre != null) {
-    res.calificacion.nombre = req.body.nombre;
-  }
-  if (req.body.apellidos != null) {
-    res.calificacion.apellidos = req.body.apellidos;
-  }
-  if (req.body.calif1 != null) {
-    res.calificacion.calif1 = req.body.calif1;
-  }
-  if (req.body.calif2 != null) {
-    res.calificacion.calif2 = req.body.calif2;
-  }
-  if (req.body.calif3 != null) {
-    res.calificacion.calif3 = req.body.calif3;
-  }
-  if (req.body.califTotal != null) {
-    res.calificacion.califTotal = req.body.califTotal;
-  }
-  if (req.body.fechaModificacion != null) {
-    res.calificacion.fechaModificacion = req.body.fechaModificacion;
-  }
-  try {
-    const updatedCalificacion = await res.calificacion.save();
-    res.json(updatedCalificacion);
-    //=======================================================
-    //REPLICANDO
-    //=======================================================
-
-    //Servidor esclavo 1
-    await fetch(`http://localhost:3002/calificaciones/${res.calificacion.noBoleta}`, {
-      method: "PATCH",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(updatedCalificacion),
-    });
-    //Servidor esclavo 2
-    await fetch(`http://localhost:3003/calificaciones/${res.calificacion.noBoleta}`, {
-      method: "PATCH",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(updatedCalificacion),
-    });
-    //=======================================================
-  } catch (error) {
-    res.status(404).json({ message: error.message });
-  }
-});
-
-//Borrando calificacion
-router.delete("/:id", getCalificacion, async (req, res) => {
-  try {
-    await res.calificacion.remove();
-    res.json({
-      message: "Calificacion eliminada",
-    });
-    //=======================================================
-    //REPLICANDO
-    //=======================================================
-
-    //Servidor esclavo 1
-    await fetch(
-      `http://localhost:3002/calificaciones/${res.calificacion.noBoleta}`,
-      {
-        method: "DELETE",
-      }
-    );
-    //Servidor esclavo 2
-    await fetch(
-      `http://localhost:3003/calificaciones/${res.calificacion.noBoleta}`,
-      {
-        method: "DELETE",
-      }
-    );
-    //=======================================================
-  } catch (error) {
-    res.status(500).json({
-      message: error.message,
-    });
-  }
-});
-
-async function getCalificacion(req, res, next) {
-  let calificacion;
-  try {
-    calificacion = await Calificacion.find({ noBoleta: req.params.id });
-    if (calificacion[0] == null) {
-      return res.status(404).json({
-        message: "No se encontro esa calificacion",
-      });
-    }
-  } catch (error) {
-    return res.status(500).json({
-      message: error.message,
-    });
-  }
-  res.calificacion = calificacion[0];
-  next();
-}
 
 module.exports = router;
