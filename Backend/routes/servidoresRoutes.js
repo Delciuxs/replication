21d87237928002e322a874ace40db63624ae2aba
CobraUltimate <carrocobra2@hotmail.com>
2021-06-16 17:48:06 -0500

    Coordinacion

--- Backend/routes/servidoresRoutes.js
+++ Backend/routes/servidoresRoutes.js
@@ -7,11 +7,12 @@ const Servidor = require("../model/servidoresModel");
 router.get("/", async (req, res) => {
   try {
     const servidores = await Servidor.find();
-    const { ip, puerto, prioridad } = req.query;
     const host = req.headers.host.split(":");
     const servidorMaster = servidores.filter(servidor => servidor.maestro)[0];
-    if(host[0] == servidorMaster.ip && host[1] == servidorMaster.puerto){
+    if(host[0] == servidorMaster.ip && host[1] == servidorMaster.puerto && req.query != undefined){
+      const { ip, puerto, prioridad } = req.query;
       if(ip != undefined && puerto != undefined && prioridad != undefined){
+        //const servidoresCaidos = [];
         var nuevoServidor = new Servidor({ip, puerto, prioridad, maestro: false});
         nuevoServidor = await nuevoServidor.save();
         const servidoresActualizados = [...servidores];
@@ -24,7 +25,18 @@ router.get("/", async (req, res) => {
               "Content-Type": "application/json",
             },
             body: JSON.stringify(nuevoServidor),
-          });
+          }).catch(error => servidoresCaidos.push(servidoresEsclavos[servidorEsclavoIndex]));
+        }
+        if(servidoresCaidos.length > 0){
+          /*const servidoresActivos = servidoresActualizados.filter(servidor => !servidor.maestro && !servidoresCaidos.includes(servidor));
+          for(var servidorCaidoIndex in servidoresCaidos){
+            const servidorCaido = servidoresCaidos[servidorCaidoIndex];
+            Servidor.remove(servidorCaido,(err) => console.log("Servidor caido removido"));
+            for(var servidorActivoIndex in servidoresActivos){
+              const servidorActivo = servidoresActivos[servidorActivoIndex];
+
+            }
+          }*/
         }
       }
     }
@@ -34,10 +46,33 @@ router.get("/", async (req, res) => {
   }
 });
 
+router.get("/ping", async (req, res) => {
+  try {
+    res.status(201).json();
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
 router.post("/", async (req, res) => {
   try {
     const servidor = new Servidor(req.body);
-    servidor = await servidor.save();
+    await servidor.save();
+    res.status(201).json(servidor);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+router.post("/coordinar", async (req, res) => {
+  try {
+    await Servidor.deleteMany({}, function(err) { 
+      console.log('collection removed') 
+    });
+    for(var index in req.body){
+      const servidor = new Servidor(req.body[index]);
+      await servidor.save();
+    }
     res.status(201).json(servidor);
   } catch (error) {
     res.status(400).json({ message: error.message });
